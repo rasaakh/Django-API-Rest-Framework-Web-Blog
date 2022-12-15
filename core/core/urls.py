f319bec489a8b88d729b5e5747df55ea403cf2c9
rasaakh <as_akh@yahoo.com>
2022-12-15 12:10:13 +0330

    5

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,7 +23,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-chema_view = get_schema_view(
+schema_view = get_schema_view(
     openapi.Info(
         title="Todo API",
         default_version="v1",
