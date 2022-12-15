9198d5406f8cf210e6092172f90bdae19ba38142
rasaakh <as_akh@yahoo.com>
2022-12-15 12:07:33 +0330

    Update urls.py
    url

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,28 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
-
-
-# from rest_framework.documentation import include_docs_urls
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+
+chema_view = get_schema_view(
     openapi.Info(
-        title="Blog Api",
+        title="Todo API",
         default_version="v1",
-        description="this is a test api for maktabkhooneh project",
+        description="project for maktabkhonneh",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="bigdeli.ali3@gmail.com"),
+        contact=openapi.Contact(email="as_akh@yahoo.com"),
         license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
 )
+
 
 
 urlpatterns = [
@@ -45,26 +45,49 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("comment/", include("comment.urls")),
     path("", include("website.urls")),
-    path("api-auth/", include("rest_framework.urls")),
-    path(
-        "swagger/output.json",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
+    
 ]
 
 
-# serving static and media for development
+if settings.COMINGSOON:
+    urlpatterns.insert(
+        0, re_path(r"^", TemplateView.as_view(template_name="comingsoon.html"))
+    )
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.SHOW_DEBUGGER_TOOLBAR:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+
+
+if settings.SHOW_SWAGGER:
+    urlpatterns += [
+        path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+        path(
+            "swagger/api.json",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        path(
+            "swagger/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "redoc/",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+    ]
+
+
+
+handler400 = "core.error_views.error_400"  # bad_request
+handler403 = "core.error_views.error_403"  # permission_denied
+handler404 = "core.error_views.error_404"  # page_not_found
+handler500 = "core.error_views.error_500"  # server_error
